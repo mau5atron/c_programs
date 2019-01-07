29a712882b99f8fee790e6b16f69424f3f14858b
Gabriel Betancourt <gabrielbetancourt96@outlook.com>
2019-01-07 12:48:00 -0500

    passing strings to functions

---
+++ codesdope/012_strings/06_passing_strings_to_functions.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void display(char str[]){
+	printf("Your string: %s\n", str);
+	// printf("Your string: ")
+	// puts(str); can also be used to print out
+}
+
+int main()
+{
+	char arr[30];
+	printf("Enter string: \n> ");
+	fgets(arr, 30, stdin); 
+	// do not use gets, it is unsafe
+	display(arr);
+	return 0;
+}
