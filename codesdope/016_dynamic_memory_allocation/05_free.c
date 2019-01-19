471ac067dd468c751f42443909e7fb8f272b0ddd
mau5atron <gabrielbetancourt@outlook.com>
2019-01-19 15:37:48 -0500

    notes for free function

---
+++ codesdope/016_dynamic_memory_allocation/05_free.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+/*
+	The 'free()' function is used to deallocate or free the memory after 
+	the program finishes which was dynamically allocated in the program 
+
+	It is advised to free the dynamically allocated memory after the 
+	program finishes so that it becomes available for future use.
+
+	void free(pointer);
+
+	This was the syntax of free function whose return type is void. Now,
+	let's see an example where we released the dynamically allocated memory at the end of the program using free.
+*/ 
+
+int main()
+{
+
+}
