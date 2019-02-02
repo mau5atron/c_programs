2195a5121f3c348c57b98fa9fa15984840b7ea0d
mau5atron <gabrielbetancourt96@outlook.com>
2019-02-02 02:07:25 -0500

    notes for dos and donts

---
+++ c_programming_one_hour_a_day/02_lesson_two/007_dos_and_donts.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+{
+	// DO
+	/*
+		DO add abundant comments to your program's source code, especially
+		near statements or functions that could be unclear to you or to 
+		someone who might have to modify it later. 
+	*/ 
+
+	// DO
+	/*
+		DO learn to  develop a style that will be helpful. A style that's too
+		lean or cryptic does not help. A style that is verbose may cause you
+		to spend more time commenting than programming. 
+	*/ 
+
+	// DONT
+	/*
+		add unnecessary comments to statements that are already clear. 
+		For example entering 
+
+		
+			The following prints hello world! 
+			printf("Hello world!\n");
+
+		lol this is pretty easy to understand, dont over-do it with comments
+	*/ 
+}
